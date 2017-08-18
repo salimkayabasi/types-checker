eae5deadbdc28f753fc0b36e2d258110c3f4c7c4
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2017-08-18 16:02:53 +0200

    update dependencies with using default options

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,51 @@
-import TypeChecker from './types-checker';
+import program from 'commander';
+import _ from 'lodash';
+import logWith from 'log-with';
+import pkg from '../package.json';
+import TypesChecker from './types-checker';
 
-TypeChecker.check();
+const logger = logWith(module);
+
+program
+  .version(pkg.version)
+  .option('-l, --log', 'Debug output. See all logs')
+  .option('-a, --all', 'Add all possible type definitions')
+  .option('-i, --interactive', 'Interactive mode')
+  .option('-N, --use-npm', 'Use NPM instead of Yarn')
+  .parse(process.argv);
+
+const options = {
+  log: program.log || (process.env.NODE_DEBUG === 'true') || false,
+  all: program.all || false,
+  interactive: program.interactive || false,
+  useNpm: program.useNpm || false,
+  cwd: program.cwd || process.cwd(),
+};
+
+if (options.log) {
+  logger.level = 'debug';
+}
+
+logger.debug('All options', options);
+
+TypesChecker.check(options)
+  .then(async (state) => {
+    logger.debug('state', state);
+    if (_.isEmpty(state)) {
+      return Promise.resolve();
+    }
+    let filtered = state;
+    if (options.interactive) {
+      filtered = await TypesChecker.interactive(options, filtered);
+      logger.debug('filtered state', filtered);
+    }
+    return TypesChecker.update(options, filtered);
+  })
+  .then(() => {
+    logger.info('done');
+    process.exit(0);
+  })
+  .catch((e) => {
+    logger.error('fatal_error', e);
+    process.exit(1);
+  });
--- src/types-checker.js
+++ src/types-checker.js
@@ -1,5 +1,78 @@
-export default class TypeChecker {
-  static check() {
+import reduce from 'async/reduce';
+import { exec } from 'child_process';
+import _ from 'lodash';
+import logWith from 'log-with';
 
+const logger = logWith(module);
+const typesPrefix = '@types/';
+
+export default class TypesChecker {
+  static async check(options) {
+    const { cwd, log } = options;
+    if (log) {
+      logger.level = 'debug';
+    }
+    const pkg = await import(`${cwd}/package.json`);
+    const dependencies = _.get(pkg, 'dependencies', {});
+    const devDependencies = _.get(pkg, 'devDependencies', {});
+    logger.debug('dependencies', dependencies);
+    logger.debug('devDependencies', devDependencies);
+    const modules = _.chain(dependencies)
+      .map((value, key) => `${typesPrefix}${key}`)
+      .difference(_.keys(devDependencies))
+      .value();
+    logger.debug('possible modules', modules);
+    return TypesChecker.checkNpm(modules);
+  }
+
+  static async checkNpm(modules) {
+    return new Promise((resolve, reject) => {
+      reduce(modules, {},
+        (response, moduleName, cb) =>
+          exec(`npm view ${moduleName} version`,
+            (err, result) => {
+              if (!err) {
+                response[moduleName] = result.trim();
+              }
+              cb(null, response);
+            }),
+        (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        },
+      );
+    });
+  }
+
+  static async interactive(options, state) {
+    return {
+      options,
+      state,
+    };
+  }
+
+  static async update(options, state) {
+    const { log, cwd, useNpm } = options;
+    const modules = _.keys(state).join(' ');
+    if (log) {
+      logger.level = 'debug';
+    }
+    if (_.isEmpty(modules)) {
+      return Promise.resolve();
+    }
+    const cmd = `${useNpm ? 'npm install --save-dev' : 'yarn add --dev'} ${modules}`;
+    logger.info('cmd', `'${cmd}'`);
+    return new Promise((done, fail) => {
+      exec(cmd, { cwd }, (err) => {
+        if (err) {
+          fail(err);
+        } else {
+          done();
+        }
+      });
+    });
   }
 }
